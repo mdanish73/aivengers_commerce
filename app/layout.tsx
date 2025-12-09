2e2a4ddd34a68946226f6a9869269a037dde710e
Muhammad Danish Iqbal <138819121+mdanish73@users.noreply.github.com>
2025-12-10 02:20:33 +0500

    Comment out WelcomeToast

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
+            {/* <WelcomeToast /> */}
           </main>
         </CartProvider>
       </body>
